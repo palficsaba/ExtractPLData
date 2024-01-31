41e7bed854fc57dee81d5777387920fbaf2583b1
palficsaba <palfi.csaba.sandor@gmail.com>
2024-01-31 10:58:47 +0200

    first commit

---
+++ ExtractPLData.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sys
+
+sys.path.append('Home/PyCharmProjects/ExtractPLData/.venv/Modules')
+import referees
+
+
+file_location = '/home/csaba/Documents/Betting/PremierLeagueTripleSolo2023-24.xlsx'
+read_info = pd.read_excel(file_location)
+
+
+
